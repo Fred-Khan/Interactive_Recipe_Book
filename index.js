e36d266fb874f618310e260622f80356326df5b7
Allan Sosa <100625448@student.kangan.edu.au>
2024-05-29 21:59:44 +1000

    Footer half way built. Along with images

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 /* JavaScript functionality for the home page */
+
+document.addEventListener("DOMContentLoaded", () => {
+    const shareLinks = [
+        { id: "share-facebook", url: "https://www.facebook.com/sharer/sharer.php?u=" },
+        { id: "share-twitter", url: "https://twitter.com/intent/tweet?url=" },
+        // Instagram sharing isn't possible via a direct URL method apperently.
+        { id: "share-instagram", action: () => alert("Use Instagram app to share this recipe.") },
+        { id: "share-youtube", action: () => alert("Share on YouTube feature is not available.") }
+    ];
+
+    shareLinks.forEach(link => {
+        const element = document.getElementById(link.id);
+        if (element) {
+            element.addEventListener("click", (e) => {
+                e.preventDefault();
+                if (link.url) {
+                    const url = window.location.href;
+                    const shareUrl = `${link.url}${encodeURIComponent(url)}`;
+                    window.open(shareUrl, "_blank");
+                } else if (link.action) {
+                    link.action();
+                }
+            });
+        }
+    });
+});
